dec94b6530eac23f33b9337571d2edc0d278372e
Avik123-del <61191261+Avik123-del@users.noreply.github.com>
2021-02-17 18:36:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+var date = new Date();                  //date object
+var presentYear = date.getFullYear();   //current year
+
+//This function computes interest and if user give 0 or -ve no then alert user.
+
+function compute(){
+    let principal = parseFloat(document.querySelector('#principal').value);
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.querySelector('#principal').focus();
+    }
+    else{
+        let rate = parseFloat(document.querySelector('#rate').value);
+        let years = parseFloat(document.querySelector('#years').value);
+        let interest = principal*years*rate/100;
+        document.getElementById('result').innerHTML="<br>If you deposit <span class='highlight'>"+principal+"</span>,<br>\
+                                                at an interest rate of <span class='highlight'>"+ rate+"%</span>.<br>\
+                                                You will receive an amount of <span class='highlight'>"+interest+"</span>,<br>\
+                                               in the year <span class='highlight'>"+ (presentYear+years) +"</span><br>";
+    }
+}
+
+
+//This function change rate of interest.
+
+function newStatus(){
+    let rate = parseFloat(document.querySelector('#rate').value);
+    document.querySelector('#status').innerHTML=rate+"%";
+}
